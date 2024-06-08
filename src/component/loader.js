7a1f4ff21c979a7957b7c7294e5cb1db422d6782
Aman Choudhary <choudhary.aman@senrysa.com>
2024-06-08 11:34:05 +0530

    worked on responsiveness

---
+++ src/component/loader.js
@@ -0,0 +1,31 @@
+import { Box, CircularProgress } from '@mui/material';
+import React from 'react';
+import { Audio } from 'react-loader-spinner'
+
+
+const Loader = () => {
+    return (
+
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            width: '100vw',
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            backgroundColor: 'rgba(0, 0, 0, 0.8)',
+            zIndex: 9999,
+            color: '#EDEADE',
+            fontSize: '2rem',
+            fontFamily: "Montserrat sans-serif",
+        }}>
+            <Box sx={{ display: 'flex' }}>
+                <CircularProgress />
+            </Box>
+        </div>
+    );
+};
+
+export default Loader;
